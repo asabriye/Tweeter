ed27903465d4ccc11bf1cc62589c96810ef3b507
asabriye <abdullahisabriye@gmail.com>
2021-11-18 21:47:15 +0000

    fixed nav bar & added icons

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,7 @@
-// const numberCounter = 140;
-//     function newCount() {
-//       const counter = $("#tweet-text").val().length;
-//       $('.counter').text(numberCounter - counter);
-//       if ($('.counter').text() < 0) {
-//         $('.counter').addClass('newclass');
-//       } else {
-//         return $('.counter').removeClass('newclass');
-//       }
-
-
-//     }
+$(document).ready(function() {
+$("#tweet-text").on('keyup', function() {
 let tweetCount = $(this).val().length;
 $("output").text(140-tweetCount);
-
-
 let $tc = $(this).closest('.input-box').siblings('.bottomline').find('.counter').text(140 - tweetCount);
 if (tweetCount <= 140) {
   $tc.removeClass('error');
@@ -21,10 +9,5 @@ if (tweetCount <= 140) {
   $tc.addClass('error');
 }
 
-
-
-// $("#tweet-text").on('keyup', function() {
-//   // console.log("checking");
-//   const tweetCount = $(this).val().length;
-//   $("output").text(140-tweetCount);
-// });
+});
+});
